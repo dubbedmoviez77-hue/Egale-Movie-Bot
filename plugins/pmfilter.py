5e8817d6d5b8d7d3084d1d48ae0adf6c7511fb2f
dubbedmoviez77-hue <dubbedmoviez77@gmail.com>
2026-06-19 21:33:48 +0530

    Update pmfilter.py

--- plugins/pmfilter.py
+++ plugins/pmfilter.py
@@ -1568,7 +1568,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
 
     elif query.data == "source":
         buttons = [[
-            InlineKeyboardButton('Source Code 📜', url='https://t.me/DM_77_help_centre_bot'),
+            InlineKeyboardButton(' Admin ', url='https://t.me/DM_77_help_centre_bot'),
             InlineKeyboardButton('⇋ ʙᴀᴄᴋ ⇋', callback_data='about')
         ]]
         reply_markup = InlineKeyboardMarkup(buttons)
